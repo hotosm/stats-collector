7dcf63e9b56de15bc6d1cfd92fdd38b4028c89e3
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-06-06 11:41:01 +0530

    aggregate projects + area

--- aggregator.js
+++ aggregator.js
@@ -1,7 +1,9 @@
 const request = require('request')
+const turf = require('turf')
 const queue = require('d3-queue').queue
 const q = queue(20)
 
+var aggregatedData = {}
 // fetch all projects
 var options = {
   url: 'https://tasks.hotosm.org/api/v1/project/search',
@@ -15,9 +17,9 @@ request(options, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     var data = JSON.parse(body)
     data.mapResults.features.forEach(function (project) {
-      console.log(project.properties.projectId)
+      // console.log(project.properties.projectId)
       q.defer(function (callback) {
-        console.log('SETTING OPTIONS FOR: ', project.properties.projectId)
+        // console.log('SETTING OPTIONS FOR: ', project.properties.projectId)
         var projectOptions = {
           url: 'https://tasks.hotosm.org/api/v1/project/' + project.properties.projectId + '?as_file=false',
           headers: {
@@ -34,8 +36,20 @@ request(options, function (error, response, body) {
     })
     q.awaitAll(function (error, results) {
       if (error) throw error
-      console.log(results)
       console.log(results.length)
+      aggregatedData['totalProjects'] = results.length
+      var area = 0;
+      results.forEach(function (project) {
+        project = JSON.parse(project)
+        var feature = {
+          "type": "Feature",
+          "properties": {}
+        }
+        feature["geometry"] = project["areaOfInterest"]
+        area = area + turf.area(feature) / 1000000
+      })
+      aggregatedData['totalArea'] = area
+      console.log(JSON.stringify(aggregatedData))
     })
   } else {
     console.log(' Error in fetching project list: ' + response.statusCode)
