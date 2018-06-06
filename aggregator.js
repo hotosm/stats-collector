409ebd70e9a775a3c60a80b2624032fbd3d7fb7f
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-06-06 12:17:24 +0530

    dependencies + home-stats

--- aggregator.js
+++ aggregator.js
@@ -4,30 +4,33 @@ const queue = require('d3-queue').queue
 const q = queue(20)
 
 var aggregatedData = {}
+
 // fetch all projects
+
 var options = {
-  url: 'https://tasks.hotosm.org/api/v1/project/search',
   headers: {
     'Accept': 'application/json',
     'Accept-Language': 'en'
   }
 }
+options.url = 'https://tasks.hotosm.org/api/v1/stats/home-page'
 
 request(options, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     var data = JSON.parse(body)
+    aggregatedData['mappersOnline'] = data['mappersOnline']
+    aggregatedData['totalTasksMapped'] = data['totalTasksMapped']
+    aggregatedData['totalMappers'] = data['totalMappers']
+  }
+})
+options.url = 'https://tasks.hotosm.org/api/v1/project/search'
+request(options, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var data = JSON.parse(body)
     data.mapResults.features.forEach(function (project) {
-      // console.log(project.properties.projectId)
       q.defer(function (callback) {
-        // console.log('SETTING OPTIONS FOR: ', project.properties.projectId)
-        var projectOptions = {
-          url: 'https://tasks.hotosm.org/api/v1/project/' + project.properties.projectId + '?as_file=false',
-          headers: {
-            'Accept': 'application/json',
-            'Accept-Language': 'en'
-          }
-        }
-        request(projectOptions, function (error, response, body) {
+        options.url = 'https://tasks.hotosm.org/api/v1/project/' + project.properties.projectId + '?as_file=false'
+        request(options, function (error, response, body) {
           if (!error && response.statusCode === 200) {
             callback(null, body)
           }
@@ -38,14 +41,14 @@ request(options, function (error, response, body) {
       if (error) throw error
       console.log(results.length)
       aggregatedData['totalProjects'] = results.length
-      var area = 0;
+      var area = 0
       results.forEach(function (project) {
         project = JSON.parse(project)
         var feature = {
-          "type": "Feature",
-          "properties": {}
+          'type': 'Feature',
+          'properties': {}
         }
-        feature["geometry"] = project["areaOfInterest"]
+        feature['geometry'] = project['areaOfInterest']
         area = area + turf.area(feature) / 1000000
       })
       aggregatedData['totalArea'] = area
