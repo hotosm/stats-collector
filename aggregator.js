023a7b1ba0a1f9ef42bb4d3e50917c6414517699
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-06-11 21:28:15 +0530

    aws integration

--- aggregator.js
+++ aggregator.js
@@ -3,8 +3,11 @@ const turf = require('turf')
 const queue = require('d3-queue').queue
 const fs = require('fs')
 const moment = require('moment')
+const AWS = require('aws-sdk')
 const q = queue(100)
 const adminBoundaries = require('./countries.json')
+var hotosmPlayGround = 'ramyaragupathy/'
+var publicAccess = 'public-read'
 var aggregatedData = {}
 var lastActive = {
   'type': 'FeatureCollection',
@@ -17,108 +20,107 @@ var options = {
   }
 }
 
-// fetch home-stats
-options.url = 'https://tasks-stage.hotosm.org/api/v1/stats/home-page'
-request(options, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    var data = JSON.parse(body)
-    aggregatedData['mappersOnline'] = data['mappersOnline']
-    aggregatedData['totalTasksMapped'] = data['tasksMapped']
-    aggregatedData['totalMappers'] = data['totalMappers']
-  }
-})
+exports.handler = function index (event, context, callback) {
+  // fetch home-stats
+  options.url = 'https://tasks-stage.hotosm.org/api/v1/stats/home-page'
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var data = JSON.parse(body)
+      aggregatedData['mappersOnline'] = data['mappersOnline']
+      aggregatedData['totalTasksMapped'] = data['tasksMapped']
+      aggregatedData['totalMappers'] = data['totalMappers']
+    }
+  })
 
-// fetch all projects
-options.url = 'https://tasks-stage.hotosm.org/api/v1/project/search'
-request(options, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    var data = JSON.parse(body)
-    data.mapResults.features.forEach(function (project) {
-      q.defer(function (callback) {
-        options.url = 'https://tasks-stage.hotosm.org/api/v1/project/' + project.properties.projectId + '?as_file=false'
-        request(options, function (error, response, body) {
-          if (!error && response.statusCode === 200) {
-            callback(null, body)
-          }
-        })
-      })
-    })
-    q.awaitAll(function (error, results) {
-      if (error) throw error
-      console.log(results.length)
-      aggregatedData['totalProjects'] = results.length
-      var area = 0
-      var count = 0
-      results.forEach(project => {
-        project = JSON.parse(project)
-        var isInside = false
-        var feature = {
-          'type': 'Feature',
-          'properties': {}
-        }
-        var projectCentroid = turf.centroid(project['areaOfInterest'])
-        console.log('projectCentroid of ' + project.projectId + ' ' + JSON.stringify(projectCentroid))
-        var currentTime = moment().format('YYYY-MM-DD[T]HH:mm:ss')
-        currentTime = moment(currentTime).utc()
-        var projectTime = moment.utc(project['lastUpdated'])
-        var diff = currentTime.diff(projectTime, 'hours')
-        if (diff <= 120) {
-          projectCentroid.properties['title'] = project.projectInfo['name']
-          projectCentroid.properties['id'] = project.projectId
-          lastActive.features.push(projectCentroid)
-        }
-        feature['geometry'] = project['areaOfInterest']
-        var projectArea = turf.area(feature) / 1000000
-        area = area + projectArea
-        feature['geometry'] = projectCentroid.geometry
-        feature.properties['title'] = project.projectInfo['name']
-        feature.properties['id'] = project.projectId
-        feature.properties['area'] = projectArea
-        feature.properties['year'] = project['lastUpdated'].slice(0, 4)
-        feature.properties['lastUpdated'] = project['lastUpdated']
-        var hashtag = project['changesetComment'].split(' ')[0].slice(1)
-        console.log(hashtag)
-        options.url = 'https://osm-stats-production-api.azurewebsites.net/stats/' + hashtag
-        request(options, function (error, response, body) {
-          console.log('Error: ', error)
-          console.log('response: ', JSON.stringify(response))
-          console.log('body: ', body)
-          if (!error) {
-            var projectStats = JSON.parse(body)
-            count++
-            console.log(projectStats)
-            feature.properties['changesets'] = projectStats.changesets
-            feature.properties['mappers'] = projectStats.users
-            feature.properties['roads'] = projectStats.roads
-            feature.properties['buildings'] = projectStats.buildings
-            feature.properties['edits'] = projectStats.edits
-            feature.properties['latest'] = projectStats.latest
-          }
-          adminBoundaries.features.forEach(boundary => {
-            isInside = turf.inside(projectCentroid, boundary)
-            if (isInside) {
-              if (!aggregatedData[boundary.properties['NAME_EN']]) {
-                aggregatedData[boundary.properties['NAME_EN']] = []
-                aggregatedData[boundary.properties['NAME_EN']].push(feature)
-              } else {
-                aggregatedData[boundary.properties['NAME_EN']].push(feature)
-              }
+  // fetch all projects
+  options.url = 'https://tasks-stage.hotosm.org/api/v1/project/search'
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var data = JSON.parse(body)
+      data.mapResults.features.forEach(function (project) {
+        q.defer(function (callback) {
+          options.url = 'https://tasks-stage.hotosm.org/api/v1/project/' + project.properties.projectId + '?as_file=false'
+          request(options, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+              callback(null, body)
             }
           })
-          aggregatedData['totalArea'] = area
-          console.log('Count: ', count)
-          fs.writeFileSync('aggregatedStats.json', JSON.stringify(aggregatedData), function (err) {
-            if (err) throw err
-            aggregatedData = {}
-          })
-          fs.writeFileSync('lastActive.json', JSON.stringify(lastActive), function (err) {
-            if (err) throw err
-            lastActive = {}
-          })
         })
       })
-    })
-  } else {
-    console.log(' Error in fetching project list: ' + response.statusCode)
-  }
-})
+      q.awaitAll(function (error, results) {
+        if (error) throw error
+        aggregatedData['totalProjects'] = results.length
+        var area = 0
+        var count = 0
+        results.forEach(project => {
+          project = JSON.parse(project)
+          var isInside = false
+          var feature = {
+            'type': 'Feature',
+            'properties': {}
+          }
+          var projectCentroid = turf.centroid(project['areaOfInterest'])
+          var currentTime = moment().format('YYYY-MM-DD[T]HH:mm:ss')
+          currentTime = moment(currentTime).utc()
+          var projectTime = moment.utc(project['lastUpdated'])
+          var diff = currentTime.diff(projectTime, 'hours')
+          if (diff <= 120) {
+            projectCentroid.properties['title'] = project.projectInfo['name']
+            projectCentroid.properties['id'] = project.projectId
+            lastActive.features.push(projectCentroid)
+          }
+          feature['geometry'] = project['areaOfInterest']
+          var projectArea = turf.area(feature) / 1000000
+          area = area + projectArea
+          feature['geometry'] = projectCentroid.geometry
+          feature.properties['title'] = project.projectInfo['name']
+          feature.properties['id'] = project.projectId
+          feature.properties['area'] = projectArea
+          feature.properties['year'] = project['lastUpdated'].slice(0, 4)
+          feature.properties['lastUpdated'] = project['lastUpdated']
+          var hashtag = project['changesetComment'].split(' ')[0].slice(1)
+          options.url = 'https://osm-stats-production-api.azurewebsites.net/stats/' + hashtag
+          request(options, function (error, response, body) {
+            if (!error) {
+              var projectStats = JSON.parse(body)
+              count++
+              feature.properties['changesets'] = projectStats.changesets
+              feature.properties['mappers'] = projectStats.users
+              feature.properties['roads'] = projectStats.roads
+              feature.properties['buildings'] = projectStats.buildings
+              feature.properties['edits'] = projectStats.edits
+              feature.properties['latest'] = projectStats.latest
+            }
+            adminBoundaries.features.forEach(boundary => {
+              isInside = turf.inside(projectCentroid, boundary)
+              if (isInside) {
+                if (!aggregatedData[boundary.properties['NAME_EN']]) {
+                  aggregatedData[boundary.properties['NAME_EN']] = []
+                  aggregatedData[boundary.properties['NAME_EN']].push(feature)
+                } else {
+                  aggregatedData[boundary.properties['NAME_EN']].push(feature)
+                }
+              }
+            })
+            aggregatedData['totalArea'] = area
+          })
+        })
+        var s3BodyGeoJSON = JSON.stringify(lastActive)
+        var s3 = new AWS.S3()
+        var uploadParams = {Bucket: 'ramyaragupathy', Key: '', Body: ''}
+        uploadParams.Body = s3BodyGeoJSON
+        uploadParams.Key = 'lastActive.json'
+        s3.upload(uploadParams, function (err, data) {
+          if (err) {
+            console.log('Error', err)
+          } if (data) {
+            console.log('Upload Success', data.Location)
+          }
+        })
+      })
+    } else {
+      console.log(' Error in fetching project list: ' + response.statusCode)
+    }
+  })
+  callback(null, aggregatedData)
+}
