c1bfd409d5569903d6904aaf0679655ce2541d23
Aruna Sankaranarayanan <aruna.evam@gmail.com>
2019-05-27 13:07:51 +0530

    add Parameters section

--- devops/cloudformation/stats-collector.template.js
+++ devops/cloudformation/stats-collector.template.js
@@ -31,4 +31,4 @@ const lambda = new cf.shortcuts.ScheduledLambda({
   ScheduleExpression: 'rate(1 hour)'
 });
 
-module.exports = cf.merge(Parameters, lambda);
+module.exports = cf.merge( { Parameters }, lambda );
