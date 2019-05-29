1ab7e3d3f6b65f0b8e569ce2e36ccda07b7049ae
S Aruna <aruna@hotosm.org>
2019-05-29 16:08:08 +0530

    Cloudformation template to set up lambda that should run on merges with master (#8)
    * Templates & other resouces from cloudfriend
    
    * iam role includes s3 bucket access
    
    * CI init
    
    * For stork
    
    * For stork
    
    * point at stork
    
    * For stork
    
    * sub
    
    * move cfn template to devops folder
    
    * merge parameters
    
    * add Parameters section
    
    * change bucket
    
    * setup circle ci config
    
    node:10
    
    add another deploy to test
    
    10.0 ->10
    
    install aws
    
    test on circleci?
    
    python3-dev
    
    sudo?
    
    staging
    
    * delete this
    
    * timeout 900s
    
    * MemorySize 1024
    
    * Mylambda -> StatsCollectorLambda

---
+++ devops/cloudformation/stats-collector.template.js
@@ -0,0 +1,36 @@
+const cf = require('@mapbox/cloudfriend');
+
+
+const Parameters = {
+  GitSha: {
+    Type: 'String'
+  }
+};
+
+const lambda = new cf.shortcuts.ScheduledLambda({
+  LogicalName: 'StatsCollectorLambda',
+  Code: {
+    S3Bucket: 'stork-us-east-1',
+    S3Key: cf.sub('bundles/stats-collector/${GitSha}.zip')
+  },
+  Statement: [
+    {
+      Effect: 'Allow',
+      Action: [
+        's3:ListBucket',
+        's3:GetObject',
+        's3:GetObjectAcl',
+        's3:ListObjects'
+      ],
+      Resource: [
+        'arn:aws:s3:::stork-us-east-1/',
+        cf.sub('arn:aws:s3:::stork-us-east-1/bundles/${GitSha}')
+      ]
+    }
+  ],
+  Timeout: 900,
+  MemorySize: 1024,
+  ScheduleExpression: 'rate(1 hour)'
+});
+
+module.exports = cf.merge( { Parameters }, lambda );
